954e79846c3faa9cf88a78d04d0e56fa0fcd447a
Adarsh Kumar <adarshkscodes@gmail.com>
2025-07-13 20:43:22 +0530

    fix: format Solidity code for CI

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -35,16 +35,14 @@ contract NFTMarketplace {
     }
 
     function buyNFT(address nft, uint256 tokenId) external payable {
-        Listing memory listing = listings[nft][tokenId];
-        if (listing.seller == address(0)) revert NotListed();
-        if (msg.value != listing.price) revert IncorrectPrice();
+    Listing memory listing = listings[nft][tokenId];
+    if (listing.seller == address(0)) revert NotListed();
+    if (msg.value != listing.price) revert IncorrectPrice();
 
-        delete listings[nft][tokenId];
+    delete listings[nft][tokenId];
 
-        IERC721(nft).transferFrom(listing.seller, msg.sender, tokenId);
+    IERC721(nft).transferFrom(listing.seller, msg.sender, tokenId);
 
-        // SAFER than `.transfer()` ✅
-        (bool sent,) = payable(listing.seller).call{value: msg.value}("");
-        require(sent, "ETH transfer failed");
-    }
+    (bool sent, ) = payable(listing.seller).call{value: msg.value}("");
+    require(sent, "ETH transfer failed");
 }
